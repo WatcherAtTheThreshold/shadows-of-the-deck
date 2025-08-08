f4865615924167f4485134a153402ac9cbfcaa40
Watcher At The Bridge <monahanhunt@gmail.com>
2025-08-08 01:50:16 -0400

    Update music.js

--- music.js
+++ music.js
@@ -19,9 +19,9 @@ class MusicManager {
 
   initializeTracks() {
     // Preload all audio tracks
-    this.tracks.start = new Audio('/music/moonlitreflections.mp3');
-    this.tracks.danger = new Audio('/music/rogue.mp3');
-    this.tracks.final = new Audio('/music/grove.mp3');
+    this.tracks.start = new Audio('./music/moonlitreflections.mp3');
+this.tracks.danger = new Audio('./music/rogue.mp3');
+this.tracks.final = new Audio('./music/grove.mp3');
 
     // Configure all tracks
     Object.values(this.tracks).forEach(track => {
