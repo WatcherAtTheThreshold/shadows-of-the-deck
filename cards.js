e9eb79f1aed65bd10a4e3cb15261a163e4ff37ee
Watcher At The Bridge <monahanhunt@gmail.com>
2025-08-21 10:48:39 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -23,6 +23,7 @@ export const CARD_EFFECTS = {
   'Phantom Step': { type: 'move', value: 2 },
   'Essence Tap': { type: 'special', effect: 'coin_and_draw', coins: 2 },
   'Spirit Guide': { type: 'special', effect: 'move_and_protect', move: 3 }
+  'Shadow (dead)': { type: 'dead', effect: 'curse' },
 };
 
 // Generate tooltips for cards
@@ -37,6 +38,8 @@ export function generateTooltip(cardName) {
       return `Move ${effect.value} spaces on the map`;
     case 'move_safe': 
       return `Move ${effect.value} spaces, skip encounters`;
+    case 'dead':  // ADD THIS CASE
+      return "Cursed card - does nothing when played";
     case 'special':
       switch (effect.effect) {
         case 'lucky_find': 
